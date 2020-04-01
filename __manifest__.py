8f1b7583a8a4c29b620ad789e60ef9bb2630474e
JosepM <jmyepes@mac.com>
2020-04-01 13:58:06 +0200

    UPD

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HelpDesk-curso",
+    'summary': "Module to Support Teams",
+    'version': '13.0.1.0.0',
+    'category': 'Customer Relationship Management',
+    'website': "",
+    'author': "Josep M Yepes <jmyepes@mac.com>",
+    'license': "AGPL-3",
+    'application': True,
+    'installable': True,
+}
