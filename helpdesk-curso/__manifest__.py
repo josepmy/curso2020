9ed05e541e3b4fdf0889226bd3fbcedd1d07f74d
JosepM <jmyepes@mac.com>
2020-04-01 15:55:03 +0200

    UPD2

--- helpdesk-curso/__manifest__.py
+++ helpdesk-curso/__manifest__.py
@@ -1,13 +1,12 @@
-# Copyright 2020 Raul Carbonell - raul.carbonell@processcontrol.es
-# License AGPL-3.0 or later
+# -*- coding: utf-8 -*-
 {
     "name": "HelpDesk Curso",
     "summary":
         "Module to Support Teams",
     "version": "13.0.1.0.0",
-    "category": "Customer Relationship Management",
+    "category": "HelpDesk",
     "website": "",
-    "author": "Raul Carbonell",
+    'author': "Josep M Yepes <jmyepes@mac.com>",
     "license": "AGPL-3",
     "data": [
         "security/helpdesk_security.xml",
--- helpdesk-curso/models/helpdesk_ticket_stage.py
+++ helpdesk-curso/models/helpdesk_ticket_stage.py
@@ -1,5 +1,4 @@
-# Copyright 2020 Raul Carbonell
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
 
